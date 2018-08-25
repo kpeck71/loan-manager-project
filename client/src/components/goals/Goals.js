8512ff5f4de1e0d4e3655941367aedc7cf3506d3
kpeck71 <kpeck71@gmail.com>
2018-08-25 12:24:50 -0400

    add bootstrap

--- client/src/components/goals/Goals.js
+++ client/src/components/goals/Goals.js
@@ -15,23 +15,32 @@ class Goals extends Component {
       // .then((data => console.log(data)))
       .then((data) => {this.setState({ goals: data }) });
   }
+
   render(){
 
     const goals = this.state.goals.map((goal) => {
       return(
-        <div key={goal.id}>
+
+        <div key={goal.id} className="col-md-3 border border-primary rounded m-2 p-1 mx-auto">
           <h3>{goal.title}</h3>
           <p>Total: {goal.total}</p>
           <p>Amount paid: {goal.amount_paid}</p>
           <p>Amount left to reach goal: {goal.amount_left}</p>
           <p>Category: {goal.category}</p>
         </div>
+
         //to add: different styling for paid vs not paid; different styling based on how amount_left
+        //border-primary - New
+        //border-success - paid 
       )
     })
     return(
       <div>
-        {goals}
+        <div className="container-fluid">
+          <div className="row">
+            {goals}
+          </div>
+        </div>
       </div>
      )
    }
