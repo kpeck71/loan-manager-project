f087b2809fcd0ff67a5798ede910951552a9ce1c
kpeck71 <kpeck71@gmail.com>
2018-08-24 20:24:10 -0400

    new Potential Goals container

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import GoalInput from './components/goals/GoalInput'
 import Goals from './containers/goals/Goals'
+import PotentialGoals from './containers/goals/PotentialGoals'
 import BudgetInput from './components/budget/BudgetInput'
 import Budget from './containers/budget/Budget'
 
@@ -14,16 +15,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to a Better Budget</h1>
         </header>
-        <p className="Goals">
-          Start by creating your goals.
-          <GoalInput />
-          <Goals />
-        </p>
-        <p className="Budget">
-          Start by creating your goals.
+        <p className="Budget"></p>
+          Start by creating your monthly budget.
           <BudgetInput />
           <Budget />
-        </p>
+        <p className="Goals">What are your budget goals?</p>
+          <GoalInput />
+          <Goals />
+        <p>Here's what's possible</p>
+          <PotentialGoals />
+
       </div>
     );
   }
---
+++ client/src/containers/goals/PotentialGoals.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PotentialGoals = props => {
+    return (
+      <div className="PotentialGoals">
+        <ul>
+          <li>Flights for under $/budgetamount/</li>
+          <li>Donate it!</li>
+          <li>Savings!</li>
+        </ul>
+      </div>
+    );
+  }
+
+export default PotentialGoals
