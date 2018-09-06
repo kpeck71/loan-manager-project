d8ba8ae0a5aae3c187875448a85680425495d215
kpeck71 <kpeck71@gmail.com>
2018-09-05 21:05:11 -0400

    removed redux tools in createStore(), was causing thunk problems

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
