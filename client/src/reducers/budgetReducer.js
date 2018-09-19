eb5c108e26d545dda347ced77d47ce495c152c47
kpeck71 <kpeck71@gmail.com>
2018-09-19 15:33:07 -0400

    remove calculateExpenses from container and add to reducer; use parseFloat to prevent concatanation of expense amounts

--- client/src/reducers/budgetReducer.js
+++ client/src/reducers/budgetReducer.js
@@ -1,8 +1,9 @@
 import cuid from 'cuid';
 
-export default function budgetReducer(state = { income: '', expenses: [] }, action) {
+export default function budgetReducer(state = { income: '', expenses: [], expenseTotal: '' }, action) {
 
 let budget;
+let expenseTotalSum;
   switch (action.type) {
   case 'ADD_BUDGET':
     return { ...state, income: action.newBudget.income }
@@ -14,8 +15,9 @@ let budget;
       amount: action.newExpense.amount,
       category: action.newExpense.category
     }
+    expenseTotalSum = parseFloat(state.expenseTotal) + parseFloat(expense.amount);
 
-    return { ...state, expenses: [...state.expenses, expense] }
+    return { ...state, expenses: [...state.expenses, expense], expenseTotal: expenseTotalSum }
 
   case 'FETCH_BUDGET':
     let budget = action.payload
@@ -24,8 +26,9 @@ let budget;
 
   case 'FETCH_EXPENSES':
     let allExpenses = action.payload
-    return {...state, expenses: allExpenses}
-
+    expenseTotalSum = 0
+    allExpenses.map((expense) => expenseTotalSum += expense.amount)
+    return {...state, expenses: allExpenses, expenseTotal: expenseTotalSum}
 
     default:
       return state
