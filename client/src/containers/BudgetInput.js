72f80b9e1dce0df3e21045c4084999cf2ae30e33
kpeck71 <kpeck71@gmail.com>
2018-09-30 19:13:55 -0400

    fix frozen Income value in budgetInput field

--- client/src/containers/BudgetInput.js
+++ client/src/containers/BudgetInput.js
@@ -3,40 +3,35 @@ import React, { Component } from 'react';
 class BudgetInput extends Component {
 
   state = {
-    income: '',
+    income: this.props.income,
     isHidden: true
   }
 
   toggleHidden() {
-    console.log('toggling')
     this.setState({
       isHidden: !this.state.isHidden
     })
   } //combine in parent and pass down to here
 
+  handleChange = event => {
+    this.setState({
+      income: event.target.value
+    })
+  }
+
   handleSubmit = event =>  {
     event.preventDefault();
-    console.log('update budget')
     this.props.updateBudget({income: this.state.income})
     this.setState({
       isHidden: true
     });
   }
 
-  handleChange = event => {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value
-    })
-  }
-
   budgetForm() {
     return (
       <div className="budget-view col-md-5 border border-primary rounded m-2 p-1 mx-auto">
         <form onSubmit={this.handleSubmit}>
-          <input type="number" name="income"placeholder="Monthly Income"  value={this.props.income} onChange={this.handleChange}/>
+          <input type="number" name="income" placeholder="Monthly Income"  value={this.state.income} onChange={this.handleChange}/>
           <input type="submit" />
         </form>
       </div>
