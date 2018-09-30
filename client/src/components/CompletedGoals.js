6898b222116db915cbc6bb02ad7694e2106bde20
kpeck71 <kpeck71@gmail.com>
2018-09-30 15:50:06 -0400

    new routes, functionality to be added later

---
+++ client/src/components/CompletedGoals.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import CardCreator from './CardCreator'
+
+const CompletedGoals = props  => {
+
+  const completedGoals = props.goals.filter((goal) => goal.paid === true)
+
+  const renderGoals =
+    completedGoals.map((goal) => {
+      return <CardCreator goal={goal} cardDetails="goalCard" goalId={goal.id}  />
+    });
+
+  return (
+    <div className="row">
+      {renderGoals}
+    </div>
+  )
+}
+
+export default CompletedGoals
---
+++ client/src/components/Status.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import CardCreator from './CardCreator'
+
+class Status extends Component {
+
+  render() {
+
+    return (
+      <div>
+
+        <p>Here is where you can improve</p>
+
+      </div>
+     )
+   }
+}
+
+export default Status
