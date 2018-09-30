85a7a1c3d69b6170c6201163c18fbb736eb9e880
kpeck71 <kpeck71@gmail.com>
2018-09-30 16:06:37 -0400

    outlining Status format for future

--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -3,12 +3,21 @@ import CardCreator from './CardCreator'
 
 class Status extends Component {
 
+  renderFun() {
+    let funTotal = 0
+    return funTotal = '$500'
+  }
+  renderEssentials() {
+    return '$1100'
+  }
+  
   render() {
 
     return (
       <div>
-
         <p>Here is where you can improve</p>
+        <p>You're spending {this.renderFun()} in the 'fun' category</p>
+        <p>You're spending {this.renderEssentials()} in the 'essentials' category</p>
 
       </div>
      )
