e97260b203ad05f86a9ec00415dee0b306888c41
kpeck71 <kpeck71@gmail.com>
2018-10-01 11:55:44 -0400

    add dropdown  options for goal categories

--- client/src/components/CardCreator.js
+++ client/src/components/CardCreator.js
@@ -1,94 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createGoal, goalPaid, deleteExpense } from '../actions/goals'
 
-const CardContainer = props => {
+class CardContainer extends Component {
 
-  let borderColor = 'info';
-  let className=`col-md-3 border border-${borderColor} rounded p-2 m-2 mx-5`
-  // let backgroundColor = 'lightgrey'
+  getClasses() {
+    let classes="col-md-3 border rounded p-2 m-2 mx-5 border-info"
+    // classes += (this.props.goal.paid === true) ? "warning" : "info"
+    // why is this.props.goal.paid undefined here?
+    return classes
+  }
 
-//   function Card({style, className='', ...rest}) {
-//   return (
-//     <div
-//       className={`col-md-3 border border-${className} rounded p-2 m-2 mx-5`}
-//       style={{paddingTop: 10, ...style}}
-//       {...rest}
-//       />
-//   )
-// }
-
-  function renderCard() {
-    const goal = props.goal
-    const expense = props.expense
-
-    if (props.cardDetails === 'goalCard') {
+  renderCard() {
+    if (this.props.cardDetails === 'goalCard') {
       return (
         <div>
-          <h4>{goal.title}</h4>
-          <p>Total: {goal.total}</p>
-          {/*}<p>Amount paid: {goal.amount_paid}</p>
-          <p>Amount left to reach goal: {goal.amount_left}</p>*/}
-          <p>Category: {goal.category}</p>
-         <div><p><a id="paid" href="#" onClick={() => props.goalPaid(goal.id, true)} >Mark Completed</a></p></div>
-         <div><p><a id="delete" href="#" onClick={() => props.deleteGoal(goal.id)} >Delete</a></p></div>
+          <h4>{this.props.goal.title}</h4>
+          <p>Total: {this.props.goal.total}</p>
+          {/*}<p>Amount paid: {this.props.goal.amount_paid}</p>
+          <p>Amount left to reach goal: {this.props.goal.amount_left}</p>*/}
+          <p>Category: {this.props.goal.category}</p>
+         <div><p><a id="paid" href="#" onClick={() => this.props.goalPaid(this.props.goal.id, true)} >Mark Completed</a></p></div>
+         <div><p><a id="delete" href="#" onClick={() => this.props.deleteGoal(this.props.goal.id)} >Delete</a></p></div>
+         {/*add an edit form for a card here */}
        </div>
       )
     }
 
-    if (props.cardDetails === 'newGoal') {
+    if (this.props.cardDetails === 'newGoal') {
       return (
       <div>
         <h3>New Goal</h3>
-        <form onSubmit={props.handleGoalSubmit}>
-          <input type="text" placeholder="Title" name="title" value={props.title} onChange={props.handleChange}/>
-          <input type="number" placeholder="Total Cost" name="total" value={props.total} onChange={props.handleChange}/>
-          <input type="text" placeholder="Category" name="category" value={props.category} onChange={props.handleChange}/>
+        <form onSubmit={this.props.handleGoalSubmit}>
+          <input type="text" placeholder="Title" name="title" value={this.props.title} onChange={this.props.handleChange}/>
+          <input type="number" placeholder="Total Cost" name="total" value={this.props.total} onChange={this.props.handleChange}/>
+{/*}<input type="text" placeholder="Category" name="category" value={this.props.category} onChange={this.props.handleChange}/> */}
+          <select name="category" value={this.props.category} onChange={this.props.handleChange}>
+            <option value="fun">Fun</option>
+            <option value="essentials">Essentials</option>
+            <option value="credit">Credit</option>
+            <option value="miscellaneous">Miscellaneous</option>
+          </select>
           <input type="submit" />
         </form>
       </div>
     )}
 
-    if (props.cardDetails === 'completedGoal') {
+    if (this.props.cardDetails === 'completedGoal') {
       return (
         <div>
-          <h4>{goal.title}</h4>
-          <p>Total: {goal.total}</p>
-          <p>Category: {goal.category}</p>
-          <a href="#" onClick={() => goalPaid(goal.id, false)}>Add to active goals?</a>
+          <h4>{this.props.goal.title}</h4>
+          <p>Total: {this.props.goal.total}</p>
+          <p>Category: {this.props.goal.category}</p>
+          <a href="#" onClick={() => goalPaid(this.props.goal.id, false)}>Add to active goals?</a>
        </div>
       )
     }
 
-    if (props.cardDetails === 'goalIdea') {
+    if (this.props.cardDetails === 'goalIdea') {
       return (
         <div>
-          <h4>{goal.title}</h4>
-          <p>Total: {goal.total}</p>
-          <p>Category: {goal.category}</p>
-          <a href="#" onClick={() => props.createGoal(goal)}>Add to your goals?</a>
+          <h4>{this.props.goal.title}</h4>
+          <p>Total: {this.props.goal.total}</p>
+          <p>Category: {this.props.goal.category}</p>
+          <a href="#" onClick={() => this.props.createGoal(this.props.goal)}>Add to your goals?</a>
         </div>
       )
     }
 
-    if (props.cardDetails === 'expenseCard') {
+    if (this.props.cardDetails === 'expenseCard') {
       return (
         <div>
-          <h2><span class="badge badge-secondary">{expense.name}</span></h2>
-          <p> Amount: {expense.amount} | Category: {expense.category} </p>
-          <p><a href="#" id="delete" onClick={() => props.deleteExpense(expense)}>Delete</a></p>
+          <h2><span class="badge badge-secondary">{this.props.expense.name}</span></h2>
+          <p> Amount: {this.props.expense.amount} | Category: {this.props.expense.category} </p>
+          <p><a href="#" id="delete" onClick={() => this.props.deleteExpense(this.props.expense)}>Delete</a></p>
         </div>
       )
     }
 
-    if (props.cardDetails === 'newExpense') {
+    if (this.props.cardDetails === 'newExpense') {
       return (
-        <form onSubmit={props.handleExpenseSubmit}>
-          <input type="text" name="name" value={props.name} placeholder="Name" onChange={props.handleChange} />
-          <input type="number" name="amount" value={props.amount} placeholder="Amount" onChange={props.handleChange} />
+        <form onSubmit={this.props.handleExpenseSubmit}>
+          <input type="text" name="name" value={this.props.name} placeholder="Name" onChange={this.props.handleChange} />
+          <input type="number" name="amount" value={this.props.amount} placeholder="Amount" onChange={this.props.handleChange} />
           <p></p>
           <label>Category:</label>
-          <select name="category" value={props.value} onChange={props.handleChange}>
+          <select name="category" value={this.props.value} onChange={this.props.handleChange}>
             <option value="fun">Fun</option>
             <option value="essentials">Essentials</option>
             <option value="credit">Credit</option>
@@ -101,12 +98,15 @@ const CardContainer = props => {
     }
 
   }
-
+render() {
   return (
-    <div className={className}>
-      {renderCard()}
+    <div className={this.getClasses()}>
+      {this.renderCard()}
     </div>
-    )
+    )}
+
 }
+
+
 
 export default connect(null, { createGoal, goalPaid, deleteExpense })(CardContainer);
